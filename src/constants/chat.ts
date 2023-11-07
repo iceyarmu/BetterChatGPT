810c67f227ee6ec0bb74ff8a76bf27a01c412d4a
xingze.he <xingze.he@me2zen.com>
2023-11-07 11:37:35 +0800

    增加新模型和定价

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -17,6 +17,7 @@ export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
   'gpt-3.5-turbo-16k',
   'gpt-4',
+  'gpt-4-1106-preview',
   // 'gpt-4-32k',
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
@@ -26,9 +27,9 @@ export const modelOptions: ModelOptions[] = [
 export const defaultModel = 'gpt-3.5-turbo';
 
 export const modelMaxToken = {
-  'gpt-3.5-turbo': 4096,
-  'gpt-3.5-turbo-0301': 4096,
-  'gpt-3.5-turbo-0613': 4096,
+  'gpt-3.5-turbo': 16384,
+  'gpt-3.5-turbo-0301': 16384,
+  'gpt-3.5-turbo-0613': 16384,
   'gpt-3.5-turbo-16k': 16384,
   'gpt-3.5-turbo-16k-0613': 16384,
   'gpt-4': 8192,
@@ -37,28 +38,29 @@ export const modelMaxToken = {
   'gpt-4-32k': 32768,
   'gpt-4-32k-0314': 32768,
   'gpt-4-32k-0613': 32768,
+  'gpt-4-1106-preview': 128000,
 };
 
 export const modelCost = {
   'gpt-3.5-turbo': {
-    prompt: { price: 0.0015, unit: 1000 },
+    prompt: { price: 0.001, unit: 1000 },
     completion: { price: 0.002, unit: 1000 },
   },
   'gpt-3.5-turbo-0301': {
-    prompt: { price: 0.0015, unit: 1000 },
+    prompt: { price: 0.001, unit: 1000 },
     completion: { price: 0.002, unit: 1000 },
   },
   'gpt-3.5-turbo-0613': {
-    prompt: { price: 0.0015, unit: 1000 },
+    prompt: { price: 0.001, unit: 1000 },
     completion: { price: 0.002, unit: 1000 },
   },
   'gpt-3.5-turbo-16k': {
-    prompt: { price: 0.003, unit: 1000 },
-    completion: { price: 0.004, unit: 1000 },
+    prompt: { price: 0.001, unit: 1000 },
+    completion: { price: 0.002, unit: 1000 },
   },
   'gpt-3.5-turbo-16k-0613': {
-    prompt: { price: 0.003, unit: 1000 },
-    completion: { price: 0.004, unit: 1000 },
+    prompt: { price: 0.001, unit: 1000 },
+    completion: { price: 0.002, unit: 1000 },
   },
   'gpt-4': {
     prompt: { price: 0.03, unit: 1000 },
@@ -83,6 +85,10 @@ export const modelCost = {
   'gpt-4-32k-0613': {
     prompt: { price: 0.06, unit: 1000 },
     completion: { price: 0.12, unit: 1000 },
+  },
+  'gpt-4-1106-preview': {
+    prompt: { price: 0.01, unit: 1000 },
+    completion: { price: 0.03, unit: 1000 },
   },
 };
 
