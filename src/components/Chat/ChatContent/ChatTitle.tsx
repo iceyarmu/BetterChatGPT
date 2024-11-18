d4fd2c94c4ace2e84a893bc363a8df6dcb2431bf
Xingze.He <xingze.he@ghoststudio.net>
2024-11-18 19:37:12 +0800

    优化模型选择

--- src/components/Chat/ChatContent/ChatTitle.tsx
+++ src/components/Chat/ChatContent/ChatTitle.tsx
@@ -3,8 +3,8 @@ import { useTranslation } from 'react-i18next';
 import { shallow } from 'zustand/shallow';
 import useStore from '@store/store';
 import ConfigMenu from '@components/ConfigMenu';
-import { ChatInterface, ConfigInterface } from '@type/chat';
-import { _defaultChatConfig } from '@constants/chat';
+import { ChatInterface, ConfigInterface, ModelOptions } from '@type/chat';
+import { _defaultChatConfig, modelOptions } from '@constants/chat';
 
 const ChatTitle = React.memo(() => {
   const { t } = useTranslation('model');
@@ -21,6 +21,7 @@ const ChatTitle = React.memo(() => {
   const setChats = useStore((state) => state.setChats);
   const currentChatIndex = useStore((state) => state.currentChatIndex);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [dropDown, setDropDown] = useState<boolean>(false);
 
   const setConfig = (config: ConfigInterface) => {
     const updatedChats: ChatInterface[] = JSON.parse(
@@ -43,18 +44,44 @@ const ChatTitle = React.memo(() => {
   return config ? (
     <>
       <div
-        className='flex gap-x-4 gap-y-1 flex-wrap w-full items-center justify-center border-b border-black/10 bg-gray-50 p-3 dark:border-gray-900/50 dark:bg-gray-700 text-gray-600 dark:text-gray-300 cursor-pointer'
+        className='flex gap-x-4 gap-y-1 flex-wrap w-full items-center justify-center border-b border-black/10 bg-gray-50 p-3 dark:border-gray-900/50 dark:bg-gray-700 text-gray-600 dark:text-gray-300 cursor-pointer'>
+        <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'
+        onClick={() => {
+          setDropDown(!dropDown);
+        }}>
+          {t('model')}: {config.model}
+          <div
+            id='dropdown'
+            className={`${
+              dropDown ? '' : 'hidden'
+            } absolute top-100 bottom-100 mt-1 z-10 bg-white rounded-lg shadow-xl border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group dark:bg-gray-800 opacity-90`}
+          >
+            <ul
+              className='text-sm text-gray-700 dark:text-gray-200 p-0 m-0'
+              aria-labelledby='dropdownDefaultButton'
+            >
+              {modelOptions.map((m) => (
+                <li
+                  className='px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer'
+                  onClick={() => {
+                    setConfig({ ...config, model: m });
+                    setDropDown(false);
+                  }}
+                  key={m}
+                >
+                  {m}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'
         onClick={() => {
           setIsModalOpen(true);
-        }}
-      >
-        <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'>
-          {t('model')}: {config.model}
-        </div>
-        <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'>
+        }}>
           {t('token.label')}: {config.max_tokens}
         </div>
-        <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'>
+        {/* <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'>
           {t('temperature.label')}: {config.temperature}
         </div>
         <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'>
@@ -65,7 +92,7 @@ const ChatTitle = React.memo(() => {
         </div>
         <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'>
           {t('frequencyPenalty.label')}: {config.frequency_penalty}
-        </div>
+        </div> */}
       </div>
       {isModalOpen && (
         <ConfigMenu
