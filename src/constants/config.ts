c43e25d8012d3fa6a2dc8dfe2a1a58af506c24e8
Xingze.He <xingze.he@ghoststudio.net>
2026-02-05 19:50:52 +0800

    Claude增加搜索功能

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -39,12 +39,14 @@ export const ModelConfigs = [
     modelName: 'claude-opus-4-5',
     apiName: 'claude-opus-4-5-20251101',
     reasoning: 'high',
+    webSearch: 'high',
     displayName: 'Claude Opus 4.5',
   },
   {
     modelName: 'claude-sonnet-4-5',
     apiName: 'claude-sonnet-4-5-20250929',
     reasoning: 'high',
+    webSearch: 'high',
     displayName: 'Claude Sonnet 4.5',
   },
   {
